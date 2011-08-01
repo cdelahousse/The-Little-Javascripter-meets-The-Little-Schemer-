afafbb85553fe1375e718863b3e7b1c5075576aa
Christian Delahousse <christian.delahousse@gmail.com>
2011-08-02 01:10:03 +0700

    organised files, added extend function, fixed readme

--- readme.markdown
+++ readme.markdown
@@ -2,11 +2,13 @@
 
 These are my solutions to [The Little Schemer](http://www.ccs.neu.edu/home/matthias/BTLS/) in Javascript using [Douglas Crockford's The Little Javascripter](http://javascript.crockford.com/little.html).
 
-Answers are divided by chapter to avoid namespace collisions. Doug wrote the same function multiple times, as the book goes. My answers are in the solutions-cxx.js files and housed within the lilJSter object.
+I've divided Doug's answers by chapter to avoid name collisions. As the book progresses, the functions are rewritten with different implementations.The files are named dc-xx.js.
+
+My answers reside in the solutions-cxx.js files. Every new chapter file creates an object that inherits the previous chapter's object.
 
 I suggest opening the HTML files to test the solutions.
 
 Word.
 
 A note on recursion in Javascript:
-Use recursion with caution. JS engines typically fail with too many levels of recursion.
+Use recursion with caution. JS engines typically fail with too many levels of recursion and do not perform tail recursion optimization. Recurring in JS is actually way slower than looping.
--- script.js
+++ script.js
@@ -1,35 +1,21 @@
-﻿//Creates 'lil Javascripter object
+﻿//Creates a new object from props that will inherit from target
+function extend (target,props) {
+
+	//Add members from props to function
+	function f (p) {
+		for (member in p){
+			this[member] = p[member];
+		};
+	}
+
+	//Inherit from target
+	f.prototype = target;
+
+	//New object
+	return new f(props);
+}
+
+//Creates 'lil Javascripter object
 lilJSter = {}
 
 _ = lilJSter //Shortcut
-
-//Test Variables
-var lat = ['a', ['b', ['c', ['b', ['d']]]]], //list of atoms
-		tup = [1,[2,[3,[4,[5]]]]],
-		num = 8,
-		atom = 'hi mom',
-		null_list = null, //See little Javascripter description
-		list_lat2 = s("((five  plums) (four) (eleven  green  oranges))"),
-		list_lat = s("((how much (wood)) could ((a (wood)  chuck)) (((chuck))) (if (a) ((wood chuck))) could  chuck wood)");
-
-//((how much (wood)) could ((a (wood)  chuck)) (((chuck))) (if (a) ((wood chuck))) could  chuck wood) 
-
-//Tests
-function test(fn,a) {
-	return a ? 
-		(function(){
-			//Functions with 2 arguments
-			console.log('Applying 2 arguments to ' + fn.name)
-			console.log('Lat: ' + fn(a, lat));
-			console.log('List of Lat: ' + fn(a, list_lat));
-		})() :
-		//Functions with one argument
-		(function(){
-			console.log('Applying single argument to ' + fn.name);
-			console.log('Lat: ' + fn(lat));
-			console.log('Atom: ' + fn(atom));
-			console.log('Number: ' + fn(num));
-			console.log('List of Lat: ' + fn(list_lat));
-			return true;
-		})();
-}
