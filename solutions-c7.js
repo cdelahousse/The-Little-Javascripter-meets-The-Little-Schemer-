2926393de0c283a6f5395d3c7c2863cbae22c997
Christian Delahousse <christian.delahousse@gmail.com>
2011-09-05 17:44:00 +0200

    more solutions

--- solutions-c7.js
+++ solutions-c7.js
@@ -24,6 +24,37 @@ lilJSter = _ = extendAndWrap(lilJSter, {
 	isSubset2 : function (s1,s2) {
 		return isNull (s1) ? true :
 			_.isMember(car(s1),s2) && _.isSubset(cdr(s1),s2);
-	}
+	},
+	isEqset: function (s1,s2) { // XXX: review
+		return isNull (s1) ? true :
+			_.isSubset(s1,s2) && _.isSubset(s2,s1) ;
+	},
+	isIntersect: function (s1,s2) {
+		return isNull (s1) ? false :
+			_.isMember(car(s1),s2) ? true :
+			/*else*/ _.isIntersect(cdr(s1),s2);
+	},
+	isIntersect2: function (s1,s2) {
+		return isNull (s1) ? false :
+			_.isMember(car(s1),s2) || _.isIntersect(cdr(s1),s2);
+	},
+	intersect : function (s1, s2) {
+		return isNull (s1) ? null :
+			_.isMember(car(s1), s2) ? cons(	car(s1),
+																			_.intersect(	cdr(s1), s2)) :
+				/*ELSE*/ _.intersect(cdr(s1),s2);
+	},
+	union : function (s1, s2) {
+		return isNull (s1) ? s2 :
+			_.isMember(car(s1),s2) ?
+				_.union(cdr(s1),s2) :
+			/*ELSE*/	cons(car(s1), _.union(cdr(s1), s2));
+	},
+	difference : function (s1, s2) {
+		return isNull (s1) ? null :
+			_.isMember(car(s1),s2) ?
+				cons(car(s1),s2) :
+			/*else*/ _.difference(cdr(s1),s2);
+	},
 });
 
